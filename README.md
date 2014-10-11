f238a53fc7d1e4d53d6034112e66835d382a0e0f
Mikola Lysenko <mikolalysenko@gmail.com>
2014-10-11 12:26:26 -0500

    readme fixups

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ void main() {
     lightDirection, 
     viewDirection, 
     normal, 
-    firberPar,
+    fiberPar,
     fiberPerp,
     shinyPar,
     shinyPerp);
--- index.glsl
+++ index.glsl
@@ -4,11 +4,22 @@ float wardSpecular(
   vec3 surfaceNormal,
   vec3 fiberParallel,
   vec3 fiberPerpendicular,
-  float shininess) {
+  float shinyParallel,
+  float shinyPerpendicular) {
 
-  //Calculate Phong power
-  vec3 R = -reflect(lightDirection, surfaceNormal);
-  return pow(max(0.0, dot(viewDirection, R)), shininess);
+  float NdotL = max(0.0, dot(surfaceNormal, lightDirection));
+  float NdotR = dot(surfaceNormal, viewDirection);
+
+  vec3 H = normalize(lightDirection + viewDirection);
+
+  float NdotH = max(0.0, dot(surfaceNormal, H));
+  float XdotH = max(0.0, dot(fiberParallel, H));
+  float YdotH = max(0.0, dot(fiberPerpendicular, H));
+
+  float coeff = sqrt(NdotL / NdotR) / (4.0 * PI * shinyParallel * shinyPerpendicular); 
+  float theta = (pow(XdotH/shinyParallel, 2.0) + pow(YdotH/shinyPerpendicular, 2.0)) / (1.0 + NdotH);
+
+  return coeff * exp(-2.0 * theta);
 }
 
-#pragma glslify: export(phongSpecular)
+#pragma glslify: export(wardSpecular)
